5753995cfaaddaa69399d92fea54c5b611e2fb82
LIPSA PATTANAIK <pattanaiklipsa11@gmail.com>
2025-11-24 01:10:15 +0530

    Add Course class with properties and methods

---
+++ src/main/java/campusbridge/models/Course.java
@@ -0,0 +1,31 @@
+package campusbridge.models;
+
+public class Course {
+
+    private String code;
+    private String title;
+    private int credits;
+
+    public Course(String code, String title, int credits) {
+        this.code = code;
+        this.title = title;
+        this.credits = credits;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getCredits() {
+        return credits;
+    }
+
+    @Override
+    public String toString() {
+        return code + ": " + title + " (" + credits + " credits)";
+    }
+}
